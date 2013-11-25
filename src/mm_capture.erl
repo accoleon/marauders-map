8239aa6cf0adb7678b793cbf86d3bc82373405fd
Kevin Xu <jxu@uoregon.edu>
2013-11-25 00:46:59 -0800

    Added mm_capture.settings to .gitignore - file will be generated by start script Fixed mm_capture to kill (SIGTERM) tshark properly on mm_capture:stop() Added startnode.sh as a start script for easing the starting up of capture nodes.

--- src/mm_capture.erl
+++ src/mm_capture.erl
@@ -1,15 +1,15 @@
 -module(mm_capture).
 -author("Kevin Xu").
--export([start/1, stop/0, init/3]).
+-export([start/0, stop/0, init/3]).
 % ExpPrg holds the args for calling tshark - capture filter is excluding beacons, display filter is excluding no mac addresses.
 % wlan[0] != 0x80 is to filter out beacon (Access Point) frames, -Y display filter is to only return rows with all 3 fields
 % wlan.sa: Source Mac Address wlan.seq: Sequence Number radiotap.dbm_antsignal: signal strength
 
 %% Reads settings from ?MODULE.settings in erlang terms
 %% Starts a ?MODULE with default settings BUT with custom node location (nodeA, nodeB, nodeC)
-start(ThisNode) ->
+start() ->
 	{ok, Terms} = file:consult("mm_capture.settings"),
-	[{tshark, ExtPrg}, {mm_receiver, Receiver}, {interface, Interface}, {blacklist, BlackList}, {whitelist, WhiteList}, _] = Terms,
+	[{tshark, ExtPrg}, {mm_receiver, Receiver}, {interface, Interface}, {blacklist, BlackList}, {whitelist, WhiteList}, {cap, ThisNode}] = Terms,
 	% Sets up interface, blacklist, whitelist, then redirects stderr to null (prevents annoying counter text)
 	String = [ExtPrg, <<" -i ">>, Interface, <<" -f \"wlan[0] != 0x80 ">>, create_blacklist_filter(BlackList), create_whitelist_filter(WhiteList), <<"\" 2> /dev/null">> ],
 	% String is probably not flattened, but open_port wants a flat string, not iolist
@@ -26,6 +26,8 @@ init(ExtPrg, Receiver, ThisNode) ->
 	register(?MODULE, self()),
 	process_flag(trap_exit, true),
 	Port = open_port({spawn, ExtPrg}, [in, exit_status, stream, {line, 255}]),
+	%{os_pid, OsPid} = erlang:port_info(Port, os_pid),
+	%io:format("~p~n", [OsPid + 1]),
 	loop(Port, Receiver, ThisNode).
 	
 %% Main ?MODULE loop
@@ -38,6 +40,9 @@ loop(Port, Receiver, ThisNode) ->
 			rpc:cast(Receiver, mm_receiver, store, [{ThisNode, {list_to_bitstring(string:left(MAC, 17)), list_to_integer(SignalStrength), list_to_integer(SeqNo), mm_misc:timestamp(microsecs)}}]),
 			loop(Port, Receiver, ThisNode);
 		stop ->
+			{os_pid, OsPid} = erlang:port_info(Port, os_pid),
+			%io:format("~p~n", [OsPid + 1]),
+			os:cmd(io_lib:format("kill -15 ~p", [OsPid + 1])),
 			erlang:port_close(Port),
 			unregister(?MODULE),
 			ok
---
+++ startnode.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+NODE=$1
+echo MM $NODE Start script
+MYIP=`ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk '{ print $2}'`
+echo Your IP address is: $MYIP
+cat mm_capture.src | sed "s/{cap, test}/{cap, $NODE}/" > src/mm_capture.settings
+cd src
+erlc mm_capture.erl mm_misc.erl
+sudo erl -name $NODE@$MYIP -setcookie mm -s mm_capture
