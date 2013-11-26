c232b8baa9cb42077caf641788b2765c415c4a2a
Andy Li <li7@uoregon.edu>
2013-11-25 20:17:42 -0800

    Minor: remove sudo requirement

--- startnode.sh
+++ startnode.sh
@@ -6,4 +6,4 @@ echo Your IP address is: $MYIP
 cat mm_capture.src | sed "s/{cap, test}/{cap, $NODE}/" > src/mm_capture.settings
 cd src
 erlc mm_capture.erl mm_misc.erl
-sudo erl -name $NODE@$MYIP -setcookie mm -s mm_capture
+erl -name $NODE@$MYIP -setcookie mm -s mm_capture
