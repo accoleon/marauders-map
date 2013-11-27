f68de19758e8431a6fee92e686b946cddd8e221a
Kevin Xu <jxu@uoregon.edu>
2013-11-26 18:24:54 -0800

    Fixed typos in mm.app.src and mm_app.erl

--- src/mm_app.erl
+++ src/mm_app.erl
@@ -1,12 +1,12 @@
 -module(mm_app).
 -behaviour(application).
+-include ("mm_records.hrl").
 
 %% API.
--export([start/2]).
--export([stop/1]).
+-export([start/2, stop/1, install/1]).
 
 %% API.
-start(_Type, _Args) ->
+start(normal, []) ->
 	Dispatch = cowboy_router:compile([
 		{'_', [
 			{"/", cowboy_static, {priv_file, mm, "index.html"}},
@@ -17,8 +17,23 @@ start(_Type, _Args) ->
 	]),
 	{ok, _} = cowboy:start_http(http, 100, [{port, 8080}],
 		[{env, [{dispatch, Dispatch}]}]),
+	%mnesia:wait_for_tables([mm_training], 5000),
 	mm_sup:start_link().
 
 stop(_State) ->
 	cowboy:stop_listener(http),
 	ok.
+
+-spec install(Nodes) -> ok | {error, Reason} when
+	Nodes :: 
+install(Nodes) ->
+	ok = mnesia:create_schema(Nodes),
+	rpc:multicall(Nodes, application, start, [mnesia]),
+	mnesia:create_table(
+		mm_training,
+		[
+			{attributes, record_info(fields, mm_training)},
+			{index, [#mm_training.timestamp]},
+			{disc_copies, Nodes}
+		]),
+	rpc:multicall(Nodes, application, stop, [mnesia]).
