01b167341e07a2882ab51ea58f993487d4ca7340
Kevin Xu <jxu@uoregon.edu>
2013-11-26 18:26:33 -0800

    Changed fields.

--- src/mm_records.hrl
+++ src/mm_records.hrl
@@ -3,8 +3,9 @@
 % hash comes from erlang:phash2({mac, seqno})
 % nodeA, nodeB, nodeC denotes the signal strengths from the respective capturenodes
 % lastupdated is a timestamp in seconds since 1970
-
-%% needs more accuracy in order to make use of multilateration - each node has its own transmitted time so we can calculate TDOA (Time Difference of Arrival)
--record(row, {hash, mac, nodeA, nodeATime, nodeB, nodeBTime, nodeC, nodeCTime, lastupdated}).
+-record(row, {hash, mac, nodeA, nodeB, nodeC, lastupdated}).
 
 -record(trainer, {mac, name}).
+
+%% Stored training data in mnesia database
+-record (mm_training, {timestamp, mac, name, x, y, nodeA, nodeB, nodeC}).
